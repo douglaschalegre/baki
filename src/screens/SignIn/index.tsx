084474a05b32fcc71fd789b6bc48935d85015515
Douglas Chalegre <douglas.chalegre@gmail.com>
2022-03-15 22:26:15 -0300

    Implementing front-end OAuth2

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -7,6 +7,12 @@ import { Button } from '../../components/Button';
 import { SignInContent } from '../../components/SignInContent';
 
 import { styles } from './styles';
+// import {
+//   CLIENT_ID,
+//   REDIRECT_URI,
+//   RESPONSE_TYPE,
+//   SCOPE,
+// } from '../../../sensitivedata';
 
 type AuthResponse = {
   type: string;
@@ -19,6 +25,14 @@ export function SignIn() {
   const navigation = useNavigation();
 
   async function handleSignIn() {
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+    try {
+      const response = await AuthSession.startAsync({ authUrl });
+      console.log(response);
+    } catch (e) {
+      // console.log(e);
+    }
+
     navigation.navigate('Trainings');
   }
 
